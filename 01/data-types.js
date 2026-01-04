365088a4a6d455600b6bf564e14cd422bf390e17
fuzzyKenny <kritiraj.tech@gmail.com>
2026-01-04 13:29:15 +0530

    Add data-type examples.

---
+++ 01/data-types.js
@@ -0,0 +1,20 @@
+let userName = "Jakie Chan";
+console.log(typeof userName); // Output => string
+
+let pi = 3.1416;
+console.log(typeof pi); // Output => number
+
+let isEasy = true;
+console.log(typeof isEasy); // Output => boolean
+
+let isNull = null;
+console.log(typeof isNull); // Output => object. Why?
+
+let isUndefined = undefined;
+console.log(typeof isUndefined);
+
+let isObject = {
+  name: "Timmy",
+  age: 21,
+};
+console.log(typeof isObject); // Output => object
